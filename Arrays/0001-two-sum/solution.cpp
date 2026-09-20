60548bb0008e1bc5dc6e462e2f50bdd01edc76ce
sakshi01-art <taragisakshi44@gmail.com>
2026-09-20 20:25:41 +0530

    feat: add optimized Two Sum solution

---
+++ Arrays/0001-two-sum/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+
+        for (int i = 0; i < (int)nums.size(); ++i) {
+            int need = target - nums[i];
+
+            if (seen.count(need)) {
+                return {seen[need], i};
+            }
+
+            seen[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
